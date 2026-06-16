e24f587b2496079549c35e238fed02abf6c3bb23
taminororo <169162271+taminororo@users.noreply.github.com>
2026-06-17 02:05:06 +0900

    feat: 期限切れを試す

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 )
 
 func main() {
 	// 手動でキャンセルできる context を作る（cancel() を呼ぶまで生き続ける）
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), 0)
 
 	// TODO(human): ctx.Err() の変化を観察する実験を書いてください。
 	//   1) cancel() を呼ぶ "前" に ctx.Err() を表示  → 何が出る？
@@ -19,5 +20,11 @@ func main() {
 	fmt.Println("errは出ない", ctx.Err())
 	cancel()
 	fmt.Println("errは出ました", ctx.Err())
+
+	if errors.Is(ctx.Err(), context.Canceled) {
+		fmt.Println("キャンセルされる")
+	} else if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+		fmt.Println("期限切れ")
+	}
 
 }
