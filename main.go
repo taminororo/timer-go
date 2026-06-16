764048f2f606c8f1f62c6f533ecad4fed067bf6c
taminororo <169162271+taminororo@users.noreply.github.com>
2026-06-17 02:10:29 +0900

    feat: カウントダウンを実装

--- main.go
+++ main.go
@@ -1,30 +1,24 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"fmt"
+	"time"
 )
 
 func main() {
-	// 手動でキャンセルできる context を作る（cancel() を呼ぶまで生き続ける）
-	ctx, cancel := context.WithTimeout(context.Background(), 0)
+	fmt.Println("カウントダウン開始！")
 
-	// TODO(human): ctx.Err() の変化を観察する実験を書いてください。
-	//   1) cancel() を呼ぶ "前" に ctx.Err() を表示  → 何が出る？
-	//   2) cancel() を呼ぶ
-	//   3) cancel() を呼んだ "後" に ctx.Err() を表示 → 何に変わった？
-	//   ヒント: fmt.Println("キャンセル前 Err():", ctx.Err()) のように出すと変化が見やすい。
-	//           生きている context の Err() は nil。
-	defer cancel()
-	fmt.Println("errは出ない", ctx.Err())
-	cancel()
-	fmt.Println("errは出ました", ctx.Err())
+	ticker := time.NewTicker(1 * time.Second)
+	count := 10
+	for range ticker.C {
 
-	if errors.Is(ctx.Err(), context.Canceled) {
-		fmt.Println("キャンセルされる")
-	} else if errors.Is(ctx.Err(), context.DeadlineExceeded) {
-		fmt.Println("期限切れ")
+		count -= 1
+		fmt.Println(count)
+		if count == 0 {
+			ticker.Stop()
+			break
+		}
 	}
 
+	fmt.Println("発射！🚀")
 }
